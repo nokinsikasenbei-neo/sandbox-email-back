de744b1e1a4dbdc7aaec4851983a601dca6e244e
melonattacker <junkitombob@gmail.com>
2022-10-02 18:07:20 +0900

    UPDATE: file return

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, UploadFile, File
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, FileResponse
 from gmail import get_service, get_message_list, get_message
 
 # from pydantic import BaseModel
@@ -15,32 +15,40 @@ def messages():
     try:
         service = get_service()
         if service == None:
-            return JSONResponse(status_code=400, content={'message': 'please issue oauth token.'})
+            return JSONResponse(status_code=400, content={'error': 'please issue oauth token.'})
         messages = get_message_list(service)
         response_json = {"messages": messages}
         return JSONResponse(status_code=200, content=response_json)
     except Exception as e:
         print(e)
-        return JSONResponse(status_code=500, content={'message': 'internal server error.'})
+        return JSONResponse(status_code=500, content={'error': 'internal server error.'})
 
 @app.get("/message/{msg_id}", tags=['Get Message'])
 def message(msg_id: str):
     try:
         service = get_service()
         if service == None:
-            return JSONResponse(status_code=400, content={'message': 'please issue oauth token.'})
+            return JSONResponse(status_code=400, content={'error': 'please issue oauth token.'})
         message = get_message(service, msg_id)
         attachment = message.get('attachment')
         if attachment != '':
             res = requests.post(SANDBOX_ENDPOINT, json={'filename': attachment}) # sandbox環境にファイルのコンパートをリクエスト
             if res.status_code != 200:
-                return JSONResponse(status_code=500, content={'message': 'internal server error.'})
+                return JSONResponse(status_code=500, content={'error': 'internal server error.'})
         message['converted'] = attachment.replace(attachment[attachment.find('.'):], '.pdf')
         response_json = {"message": message}
         return JSONResponse(status_code=200, content=response_json)
     except Exception as e:
         print(e)
         return JSONResponse(status_code=500, content={'message': 'internal server error.'})
+
+@app.get("/file", tags=['Get File'])
+def file(name: str):
+    # filename validation
+    for char in name:
+        if (char.isalnum() == False) and (char != '.'):
+            return JSONResponse(status_code=400, content={'error': 'invalid request.'})
+    return FileResponse(path=f"/var/public/converted/{name}")
 
 @app.post("/messages/send", tags=['Send Message'])
 def send_email(raw: str, file: UploadFile = File(...)):
